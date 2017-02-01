483c30c9f0eaf9d173975cfedec6188fcd8ff1bf
Patrik Björk <patrik.bjork@codespirit.se>
2017-02-01 09:37:17 +0100

    First commit after first generation of javg template.

---
+++ core-bc/modules/web/src/main/java/se/vgregion/handbok/controller/MyController.java
@@ -0,0 +1,25 @@
+package se.vgregion.handbok.controller;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
+import org.springframework.stereotype.Component;
+
+import java.io.Serializable;
+
+/**
+ * @author Patrik Björk
+ */
+@Component(value = "myController")
+@Scope(value = "view", proxyMode = ScopedProxyMode.TARGET_CLASS)
+public class MyController implements Serializable {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
